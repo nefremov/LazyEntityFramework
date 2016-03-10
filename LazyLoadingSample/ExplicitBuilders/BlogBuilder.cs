ceab45667d4b560d013d7c0713939b991cecf2c2
Nick Efremov <nefremov@gmail.com>
2016-03-10 08:45:12 +0300

    Append lost files

---
+++ LazyLoadingSample/ExplicitBuilders/BlogBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LazyEntityFrameworkCore.Encapsulation.Builders;
+using LazyLoadingSample.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace LazyLoadingSample.Builders
+{
+    public class BlogBuilder : EntityBuilder<Blog>
+    {
+        private DbContext _Context;
+
+        public BlogBuilder(DbContext context) : base(context)
+        {
+            _Context = context;
+            Requires(b => b.Url);
+            //Requires(b => b.BlogId);
+        }
+
+        protected override Blog Construct()
+        {
+            var blog = base.Construct();
+            _Context.Add(blog);
+            return blog;
+        }
+    }
+}
---
+++ LazyLoadingSample/ExplicitBuilders/PostBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using LazyEntityFrameworkCore.Encapsulation.Builders;
+using LazyLoadingSample.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LazyLoadingSample.Builders
+{
+    public class PostBuilder : EntityBuilder<Post>
+    {
+        private DbContext _Context;
+        public PostBuilder(DbContext context) : base(context)
+        {
+            _Context = context;
+            Requires(b => b.Content);
+        }
+
+        protected override Post Construct()
+        {
+            var content = Get(p => p.Content);
+
+            var instExp = Expression.Parameter(typeof(Post));
+            var fieldExp = Expression.Field(instExp, typeof(Post).GetTypeInfo().GetDeclaredField("_Blog"));
+            var expr = Expression.Lambda<Func<Post, Blog>>(fieldExp, instExp);
+            var blog = Get(expr);
+
+            var post = new Post(blog, content);
+            ApplySetters(post);
+            _Context.Add(post);
+            return post;
+        }
+    }
+}
