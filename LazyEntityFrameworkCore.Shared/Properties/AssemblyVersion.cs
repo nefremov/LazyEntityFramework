faf3bb408ec01f442596fbb8fd2cf775ece669dc
Nikolay Efremov <Efremov@competentum.ru>
2016-06-06 17:14:02 +0300

    Use common AssemblyVersion.cs. Increase packages' version

--- LazyEntityFrameworkCore.Shared/Properties/AssemblyVersion.cs
+++ LazyEntityFrameworkCore.Shared/Properties/AssemblyVersion.cs
@@ -4,12 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LazyEntityFramework.Shared")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LazyEntityFramework.Shared")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Nick Efremov")]
+[assembly: AssemblyProduct("LazyEntityFrameworkCore")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,6 +15,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ef0c3ea-19b6-4b60-8256-856cb1f81931")]
